7d45eb575352daffaeda0df87548932c18877dca
lisagroe <lisagroeblinger@gmail.com>
2023-04-22 16:00:27 +0200

    implemented unit conversion

--- src/app.js
+++ src/app.js
@@ -58,19 +58,56 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+  celsiusTemp = response.data.main.temp;
+  celsiusFeelTemp = response.data.main.feels_like;
 }
 
 function search(city) {
-let apiKey = "dac7ababb64b3ca8acde2e00719b2bea";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(displayTemperature);
+  let apiKey = "dac7ababb64b3ca8acde2e00719b2bea";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
 }
 
 function handleSubmit(event) {
   event.preventDefault();
   let cityInputElement = document.querySelector("#city-input");
-  search(cityInputElement.value)
+  search(cityInputElement.value);
 }
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
+
+function displayFahrenheit(event) {
+  event.preventDefault();
+  let fahrenheitTemp = (celsiusTemp * 9) / 5 + 32;
+  let temperatureElement = document.querySelector("#currentTemp");
+  let temperatureFeelElement = document.querySelector("#currentFeel");
+  let fahrenheitFeelTemp = (celsiusFeelTemp * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemp);
+  temperatureFeelElement.innerHTML = `feels like: ${Math.round(
+    fahrenheitFeelTemp
+  )}°F`;
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+}
+
+function displayCelsius(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#currentTemp");
+  temperatureElement.innerHTML = Math.round(celsiusTemp);
+  let temperatureFeelElement = document.querySelector("#currentFeel");
+  temperatureFeelElement.innerHTML = `feels like: ${Math.round(
+    celsiusFeelTemp
+  )}°C`;
+  fahrenheitLink.classList.remove("active");
+  celsiusLink.classList.add("active");
+}
+
+let celsiusTemp = null;
+let celsiusFeelTemp = null;
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheit);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsius);
